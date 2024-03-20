7ea134680795ca630b1e43ec6fc72b4dbcf74289
R4gd0ll <93909093+R4gd0ll@users.noreply.github.com>
2024-03-20 15:37:47 +0800

    Delete src directory

--- src/main/java/core/CryptInterface.java
+++
@@ -1,8 +0,0 @@
-package core;
-
-import javafx.scene.control.TextArea;
-
-public interface CryptInterface {
-    Boolean decrypt(String text,String type, TextArea textArea_decrypt,TextArea textArea_info);
-    Boolean encrypt(String text,String type, TextArea textArea_encrypt,TextArea textArea_info);
-}
